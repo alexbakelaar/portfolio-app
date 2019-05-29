46301931bc280a70db4acf5b623a1437af2afac4
AlexBakelaarDev <alexbakelaar@gmail.com>
2019-05-29 13:56:24 -0400

    Updates

---
+++ precache-manifest.25f7294e17fa4da997ca3e832135fce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca3c1cedb71f4fa7552d048e9f90a0a",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "2f1511310d21dc9d7d75",
+    "url": "/portfolio-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2f1511310d21dc9d7d75",
+    "url": "/portfolio-app/static/js/main.574a8f32.chunk.js"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
